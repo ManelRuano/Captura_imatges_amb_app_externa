03232ab43629cc4c0dd2ab51af1f6c77c18dea89
ManelRuano <ruano.garcia.manel@gmail.com>
2023-12-11 16:41:18 +0100

    pr 2.1

---
+++ app/src/main/java/com/example/galery/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.galery;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private ActivityResultLauncher<Intent> someActivityResultLauncher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+
+        Button buttonOpenGallery = findViewById(R.id.buttonOpenGallery);
+        buttonOpenGallery.setOnClickListener(v -> openGallery());
+
+        someActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                        Uri selectedImageUri = result.getData().getData();
+                        if (selectedImageUri != null) {
+                            imageView.setImageURI(selectedImageUri);
+                        }
+                    }
+                });
+    }
+
+    private void openGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        someActivityResultLauncher.launch(intent);
+    }
+}
